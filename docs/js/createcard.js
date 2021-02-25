0b6618792c71339e899d7d09086880351e6e83bd
Anthony Zhou <anthony.zhou03@gmail.com>
2021-02-24 23:39:33 -0600

    Added some conditional logic to show different text on the QR code page

--- docs/js/createcard.js
+++ docs/js/createcard.js
@@ -1,7 +1,32 @@
 const BASE_URL = 'https://anthony-zhou.github.io/scannable-contact-card/download';
-const ContactForm = document.getElementById('contact-form');
+const ContactBox = document.getElementById('form-box');
+const QRCodeBox = document.getElementById('qrcode-box');
 
-function displayQRCode(vcfContent) {
+function utf8_to_b64( str ) {
+  return window.btoa(unescape(encodeURIComponent( str )));
+}
+
+function b64_to_utf8( str ) {
+  return decodeURIComponent(escape(window.atob( str )));
+}
+
+function generateVcfContent({ firstName, lastName, phoneNumber, emailAddress }) {
+  const timestamp = new Date().toISOString();
+
+  return `BEGIN:VCARD
+VERSION:3.0
+N:${lastName};${firstName}
+TEL;TYPE=MOBILE,VOICE:${phoneNumber}
+EMAIL:${emailAddress}
+REV:${timestamp}
+END:VCARD`;
+}
+
+function displayQRCode(contactInfo) {
+  const decodedContactInfo = JSON.parse(b64_to_utf8(contactInfo));
+  const fullName = `${decodedContactInfo.firstName} ${decodedContactInfo.lastName}`;
+  document.getElementById('full-name').innerText = fullName;
+  const vcfContent = generateVcfContent(decodedContactInfo);
   const contactUrl = `${BASE_URL}?vcfContent=${encodeURIComponent(vcfContent)}`;
   new QRCode(document.getElementById('qrcode'), contactUrl);
 }
@@ -19,32 +44,25 @@ function getFormFields() {
   };
 }
 
-function generateVcfContent({ firstName, lastName, phoneNumber, emailAddress }) {
-  const timestamp = new Date().toISOString();
-
-  return `BEGIN:VCARD
-VERSION:3.0
-N:${lastName};${firstName}
-TEL;TYPE=MOBILE,VOICE:${phoneNumber}
-EMAIL:${emailAddress}
-REV:${timestamp}
-END:VCARD`;
-}
 
 function loadQRCode(contactInfo) {
-  const vcfContent = generateVcfContent(contactInfo);
-  document.location.href = `?vcfContent=${encodeURIComponent(vcfContent)}`;
+  const base64ContactInfo = utf8_to_b64(JSON.stringify(contactInfo));
+  document.location.href = `?contact=${base64ContactInfo}`;
+}
+
+function showCode() {
+  QRCodeBox.style.display = 'block';
 }
 
 function showForm() {
-  ContactForm.style.display = 'block';
+  ContactBox.style.display = 'block';
 }
 
 function hideForm() {
-  ContactForm.style.display = 'none';
+  ContactBox.style.display = 'none';
 }
 
-ContactForm.addEventListener('submit', (e) => {
+document.getElementById('contact-form').addEventListener('submit', (e) => {
   e.preventDefault();
   hideForm();
   loadQRCode(getFormFields());
