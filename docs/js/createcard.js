5ade031b82a053bf8d9a52e157c221eed1f19156
Anthony Zhou <anthony.zhou03@gmail.com>
2021-02-24 21:59:25 -0600

    Moved script into separate file

---
+++ docs/js/createcard.js
@@ -0,0 +1,51 @@
+const BASE_URL = 'https://anthony-zhou.github.io/scannable-contact-card';
+const ContactForm = document.getElementById('contact-form');
+
+function displayQRCode(vcfContent) {
+  const contactUrl = `${BASE_URL}/?vcfContent=${encodeURIComponent(vcfContent)}`;
+  new QRCode(document.getElementById('qrcode'), contactUrl);
+}
+
+function getFormValue(id) {
+  return document.getElementById(id).value;
+}
+
+function getFormFields() {
+  return {
+    firstName: getFormValue('fname'),
+    lastName: getFormValue('lname'),
+    phoneNumber: getFormValue('phone'),
+    emailAddress: getFormValue('email')
+  };
+}
+
+function generateVcfContent({ firstName, lastName, phoneNumber, emailAddress }) {
+  const timestamp = new Date().toISOString();
+
+  return `BEGIN:VCARD
+VERSION:3.0
+N:${lastName};${firstName}
+TEL;TYPE=MOBILE,VOICE:${phoneNumber}
+EMAIL:${emailAddress}
+REV:${timestamp}
+END:VCARD`;
+}
+
+function loadQRCode(contactInfo) {
+  const vcfContent = generateVcfContent(contactInfo);
+  document.location.href = `createcard.html?vcfContent=${encodeURIComponent(vcfContent)}`;
+}
+
+function showForm() {
+  ContactForm.style.display = 'block';
+}
+
+function hideForm() {
+  ContactForm.style.display = 'none';
+}
+
+ContactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  hideForm();
+  loadQRCode(getFormFields());
+})
